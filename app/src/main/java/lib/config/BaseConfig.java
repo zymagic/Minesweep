52dad4d919a0ed3581938a6dead44c14ec357262
zy <zy@zydeMacBook-Pro.local>
2017-07-20 14:21:21 +0800

    删除无用的类

--- app/src/main/java/lib/config/BaseConfig.java
+++
@@ -1,8 +0,0 @@
-package lib.config;
-
-/**
- * Created by j-zhangyang5 on 2017/1/16.
- */
-
-public class BaseConfig {
-}
--- app/src/main/java/lib/http/Http.java
+++
@@ -1,8 +0,0 @@
-package lib.http;
-
-/**
- * Created by j-zhangyang5 on 2017/1/16.
- */
-
-public class Http {
-}
